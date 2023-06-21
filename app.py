395d5048d9ee9d9f1dc6f3a9753c2aa81019ed79
Geran Wen <geranwen@gmail.com>
2023-06-21 18:19:21 -0400

    incorporated filter buttons mvp

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-### RUN THIS UNDER 'trt_tennis' ENVIRONMENT
-###
-
 import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
 from dash import dcc
 from dash import html
-
 import dash_leaflet as dl
 import pandas as pd
 from dash.dependencies import Input, Output
@@ -18,7 +12,7 @@ df = pd.read_csv('tennis_courts_toronto_full_cleaned_v2.csv')
 # Create a Dash instance
 app = dash.Dash(__name__)
 
-# add this line to exposre the flask server instance as the server. 
+# add this line to expose the flask server instance as the server. 
 server = app.server
 
 # Define the layout
@@ -32,7 +26,39 @@ app.layout = html.Div(children=[
         ),
         html.Div(id='search-output')
     ]),
-    dl.Map(id='map', center=[df['latitude'].mean(), df['longitude'].mean()], zoom=10, children=[
+    html.Div([
+        html.Label('Select Type:'),
+        dcc.Dropdown(
+            id='type-dropdown',
+            options=[{'label': i, 'value': i} for i in df['type'].unique()],
+            multi=True
+        ),
+    ]),
+    html.Div([
+        html.Label('Select Lights:'),
+        dcc.Dropdown(
+            id='lights-dropdown',
+            options=[{'label': i, 'value': i} for i in df['lights'].unique()],
+            multi=True
+        ),
+    ]),
+    html.Div([
+        html.Label('Select Courts:'),
+        dcc.Dropdown(
+            id='courts-dropdown',
+            options=[{'label': i, 'value': i} for i in df['courts'].unique()],
+            multi=True
+        ),
+    ]),
+    html.Div([
+        html.Label('Winter Play Available:'),
+        dcc.Dropdown(
+            id='winter-play-dropdown',
+            options=[{'label': i, 'value': i} for i in df['winter_play'].dropna().unique()],
+            multi=True
+        ),
+    ]),
+    dl.Map(id='map', center=[df['latitude'].mean(), df['longitude'].mean()], zoom=11, children=[
         dl.TileLayer(),
     ], style={'width': '100%', 'height': '85vh', 'margin': "auto", "display": "block"}),
 ])
@@ -40,9 +66,25 @@ app.layout = html.Div(children=[
 # Update the map based on the search input
 @app.callback(
     Output('map', 'children'),
-    [Input('search-input', 'value')]
+    [Input('search-input', 'value'),
+     Input('type-dropdown', 'value'),
+     Input('lights-dropdown', 'value'),
+     Input('courts-dropdown', 'value'),
+     Input('winter-play-dropdown', 'value')]
 )
-def update_output(value):
+def update_output(value, selected_types, selected_lights, selected_courts, selected_winter_play):
+    if selected_types is None:
+        selected_types = df['type'].unique().tolist()
+    if selected_lights is None:
+        selected_lights = df['lights'].unique().tolist()
+    if selected_courts is None:
+        selected_courts = df['courts'].unique().tolist()
+    if selected_winter_play is None:
+        selected_winter_play = df['winter_play'].dropna().unique().tolist()
+
+    filtered_df = df[df['type'].isin(selected_types) & df['lights'].isin(selected_lights) 
+                     & df['courts'].isin(selected_courts) & df['winter_play'].isin(selected_winter_play)]
+
     if value is None or value == '':
         # If the search box is empty, display all markers
         markers = [
@@ -52,18 +94,20 @@ def update_output(value):
                         html.Div(f"Place: {place}"),
                         html.Div(f"Type: {typ}"),
                         html.Div(f"Lights: {lights}"),
-                        html.Div(f"Courts: {courts}")
+                        html.Div(f"Courts: {courts}"),
+                        html.Div(f"Winter Play: {winter_play if pd.notna(winter_play) else 'No'}")
                     ])
                 ])
-            ]) for lat, lon, place, typ, lights, courts in zip(
-                df['latitude'], df['longitude'], df['place'], df['type'], df['lights'], df['courts']
+            ]) for lat, lon, place, typ, lights, courts, winter_play in zip(
+                filtered_df['latitude'], filtered_df['longitude'], filtered_df['place'], filtered_df['type'], 
+                filtered_df['lights'], filtered_df['courts'], filtered_df['winter_play']
             )
         ]
     else:
         # If there's a search term, find the best match and display only that marker
-        best_match = process.extractOne(value, df['place'].tolist())
+        best_match = process.extractOne(value, filtered_df['place'].tolist())
         if best_match:
-            matched_row = df[df['place'] == best_match[0]]
+            matched_row = filtered_df[filtered_df['place'] == best_match[0]]
             markers = [
                 dl.Marker(position=[matched_row['latitude'].values[0], matched_row['longitude'].values[0]], children=[
                     dl.Tooltip(children=[
@@ -71,7 +115,8 @@ def update_output(value):
                             html.Div(f"Place: {matched_row['place'].values[0]}"),
                             html.Div(f"Type: {matched_row['type'].values[0]}"),
                             html.Div(f"Lights: {matched_row['lights'].values[0]}"),
-                            html.Div(f"Courts: {matched_row['courts'].values[0]}")
+                            html.Div(f"Courts: {matched_row['courts'].values[0]}"),
+                            html.Div(f"Winter Play: {matched_row['winter_play'].values[0] if pd.notna(matched_row['winter_play'].values[0]) else 'No'}")
                         ])
                     ])
                 ])
